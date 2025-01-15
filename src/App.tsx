0bdab314a35c8729efc9144e909b008e0c36238e
Caroline de Moraes <109109668+carolinedemoraes@users.noreply.github.com>
2025-01-15 14:41:28 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import workInProgressLogo from './assets/carol_at_work.png'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <img src={workInProgressLogo} className="workInProgressLogo" alt="Carol at Work" />
+      </div>
+    </>
+  )
+}
+
+export default App
